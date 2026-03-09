ae133c9a678b3bc5091387da4bc192ca02c50738
Sergio <sergiohijes@gmail.com>
2026-03-09 16:13:35 +0100

    no terminado

---
+++ persona/api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+from django.db import migrations, models
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
